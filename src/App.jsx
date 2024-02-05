afe2a59be4231689b517fb02a0b96df22f398a08
Sah-Abhishek <abhisheksah3333@gmail.com>
2024-02-05 05:37:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+
+
+
+`
+
+function App() {
+
+  return (
+    <>
+      <button>
+        This is styled component button
+      </button>
+    </>
+  )
+}
+
+export default App
